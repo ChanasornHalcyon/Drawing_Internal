c006d1451b8d4e03b4b6417a61b97b8644f95e79
Chanasorn <chanasorn@example.com>
2026-01-06 14:25:13 +0700

    Roll back

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ const initMySQL = async () => {
 };
 initMySQL();
 const transporter = nodemailer.createTransport({
-  host: "192.168.23.8",
+  host: "mail.halcyon.local",
   port: 587,
   secure: false, 
   auth: {
